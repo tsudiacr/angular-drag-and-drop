9e330fd51967ef5bc9ef64eebd46101392ccf9d6
tsudiacr <diana_melis_sa@hotmail.com>
2021-01-21 18:03:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dnpwkl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dnpwkl)
---
+++ src/app/cdk-drag-drop-connected-sorting-group-example.ts
@@ -0,0 +1,75 @@
+import { Component } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
+
+/**
+ * @title Drag&Drop connected sorting group
+ */
+@Component({
+  selector: "cdk-drag-drop-connected-sorting-group-example",
+  templateUrl: "cdk-drag-drop-connected-sorting-group-example.html",
+  styleUrls: ["cdk-drag-drop-connected-sorting-group-example.css"]
+})
+export class CdkDragDropConnectedSortingGroupExample {
+  todo = ["1234", "7867", "4567", "9674"];
+
+  done = [];
+  done2 = [];
+  done3 = [];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+  drop2(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+  drop3(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+}
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
